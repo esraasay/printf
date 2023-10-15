6aea1a22904e41354d6f6c0ebf65e56c4069abca
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-15 05:49:29 -0700

    printf task 0 c program

---
+++ 0-print_%cs.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+  * _printf - produces output according to a format.
+  * @format: a character string.
+  * Return: number of characters
+  */
+
+int _printf(const char *format, ...)
+{
+	int i;
+	char c, *s;
+	va_list argument;
+
+	va_start(argument, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				c = va_arg(argument, int);
+				write(1, &c, 1);
+				format++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				s = va_arg(argument, char*);
+				write(1, s, strlen(s));
+				format++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				write(1, &format[i], 1);
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+	}
+	va_end(argument);
+	return (*format);
+}
